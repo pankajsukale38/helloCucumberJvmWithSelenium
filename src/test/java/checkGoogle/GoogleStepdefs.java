163bdd5727b709165e9a675479b15ee753c3f7fe
Rafael Walter <rafawalter@gmail.com>
2013-06-14 23:50:17 -0300

    fluent-selenium

--- src/test/java/checkGoogle/GoogleStepdefs.java
+++ src/test/java/checkGoogle/GoogleStepdefs.java
@@ -1,6 +1,5 @@
 package checkGoogle;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.pt.Dado;
@@ -8,20 +7,20 @@ import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.seleniumhq.selenium.fluent.FluentWebDriver;
+import org.seleniumhq.selenium.fluent.FluentWebElement;
+import org.seleniumhq.selenium.fluent.Period;
 
 public class GoogleStepdefs {
     private WebDriver driver;
+    private FluentWebDriver fluentWebDriver;
 
     @Before
     public void before() {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver 2");
         driver = new ChromeDriver();
+        fluentWebDriver = new FluentWebDriver(driver);
     }
 
     @After
@@ -32,27 +31,40 @@ public class GoogleStepdefs {
     @Dado("^que acessei o motor de busca$")
     public void que_acessei_o_motor_de_busca() throws Throwable {
         driver.get("http://www.google.com");
-        System.out.println(driver.getTitle());
+
+        fluentWebDriver.within(Period.secs(10)).title().shouldBe("Google");
+/*
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
                 return d.getTitle().toLowerCase().startsWith("google");
             }
         });
+*/
     }
 
     @Quando("^pesquiso por Cucumber$")
     public void pesquiso_por_Cucumber() throws Throwable {
+        final FluentWebElement searchTextElement = fluentWebDriver.input(By.name("q"));
+        searchTextElement.sendKeys("Cucumber");
+        searchTextElement.submit();
+
+        /*
         WebElement searchTextElement = driver.findElement(By.name("q"));
         searchTextElement.sendKeys("Cucumber");
         searchTextElement.submit();
+        */
     }
 
     @Entao("^encontro diversos links$")
     public void encontro_diversos_links() throws Throwable {
+        fluentWebDriver.within(Period.secs(10)).title().shouldMatch("^cucumber.*$");
+
+        /*
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
                 return d.getTitle().toLowerCase().startsWith("cucumber");
             }
         });
+        */
     }
 }
