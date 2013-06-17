afb7ca91193c7c2146df8328274e0f2d8b585438
Rafael Walter <rafawalter@gmail.com>
2013-06-17 00:51:59 -0300

    seleniumHubUrl = System.getProperty("selenium.hub.url", System.getenv("SELENIUM_HUB_URL"))

--- src/test/java/checkGoogle/WebDriverFactory.java
+++ src/test/java/checkGoogle/WebDriverFactory.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.UnreachableBrowserException;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverFactory {
 
-    public static WebDriver getRemoteChromeDriver() {
+    public static WebDriver getRemoteChromeDriver(String seleniumHubUrl) throws MalformedURLException {
         DesiredCapabilities capability = DesiredCapabilities.chrome();
-        return new RemoteWebDriver(capability);
+        return new RemoteWebDriver(new URL(seleniumHubUrl), capability);
     }
 
     public static WebDriver getLocalChromeDriver() {
@@ -20,13 +22,25 @@ public class WebDriverFactory {
 
     public static WebDriver getSmartChromeDriver() {
         WebDriver driver;
-        try {
-            driver = getRemoteChromeDriver();
-        } catch (UnreachableBrowserException e) {
+        String seleniumHubUrl = getSeleniumHubUrl();
+
+        if (seleniumHubUrl != null) {
+            try {
+                System.out.println("SELENIUM_HUB em " + seleniumHubUrl);
+                driver = getRemoteChromeDriver(seleniumHubUrl);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            System.out.println("SELENIUM_LOCAL");
             driver = getLocalChromeDriver();
         }
 
         return driver;
+    }
+
+    public static String getSeleniumHubUrl() {
+        return System.getProperty("selenium.hub.url", System.getenv("SELENIUM_HUB_URL"));
     }
 
 }
