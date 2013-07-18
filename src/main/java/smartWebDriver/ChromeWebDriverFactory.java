10c658124270b803b147dce8fdde8516160c278e
Rafael Walter <rafawalter@gmail.com>
2013-07-18 01:02:42 -0300

    ChromeDriver for multiple OSses

--- src/main/java/smartWebDriver/ChromeWebDriverFactory.java
+++ src/main/java/smartWebDriver/ChromeWebDriverFactory.java
@@ -1,5 +1,6 @@
 package smartWebDriver;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -15,7 +16,17 @@ public class ChromeWebDriverFactory extends AbstractWebDriverFactory {
 
     @Override
     public WebDriver getLocalWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver 2");
+        String driverUri;
+        if (SystemUtils.IS_OS_MAC) {
+            driverUri = "./bin/chromedriver_mac/chromedriver 3";
+        } else if (SystemUtils.IS_OS_LINUX) {
+            driverUri = "./bin/chromedriver_linux/chromedriver";
+        } else if (SystemUtils.IS_OS_WINDOWS) {
+            driverUri = "./bin/chromedriver_win/chromedriver.exe";
+        } else {
+            throw new RuntimeException("Usupported Operationg System [" + SystemUtils.OS_NAME + "]");
+        }
+        System.setProperty("webdriver.chrome.driver", driverUri);
         return new ChromeDriver();
     }
 }
